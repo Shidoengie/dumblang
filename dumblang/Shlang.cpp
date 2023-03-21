43283ef4a57ee69b770b3597d1c62cf9f9584f2d
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2023-03-21 09:14:14 +0000

    split tokens from scanner

---
+++ dumblang/Shlang.cpp
@@ -0,0 +1,139 @@
+﻿#include <variant>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include "Token.h"
+std::string source;
+int current = 0;
+int line = 0;
+
+
+
+static std::vector<Token> TokenStream = {};
+
+void addToken(Token::Type type, std::variant<double, std::string> lexeme)
+{
+    Token token = Token(type, lexeme, line);
+    TokenStream.push_back(token);
+}
+bool isAtEnd() {
+    return current >= source.length();
+}
+char advance() {
+    return source[current++];
+}
+char peek() {
+    return source[current];
+}
+char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source[current + 1];
+}
+void num(char LastChar) {
+    std::string numStr;
+    do {
+        numStr += LastChar;
+        LastChar = advance();
+    } while (isdigit(LastChar) || LastChar == '.');
+    current--;
+    double num = std::stod(numStr);
+    addToken(Token::NUM, num);
+}
+
+void str() {
+    if (isAtEnd()) {
+        return;
+    }
+    char LastChar = advance();
+    std::string endStr;
+    do {
+        endStr += LastChar;
+        LastChar = advance();
+    } while (LastChar != '"');
+    addToken(Token::STR, endStr);
+}
+void identifier(char LastChar) {
+    std::string endStr;
+    do {
+        endStr += LastChar;
+        LastChar = advance();
+    } while (isalnum(LastChar));
+    
+    if (mapKeyword(endStr)) {
+        if (mapKeyword(endStr) == Token::TRUE) {
+            addToken(Token::TRUE, 1.0);
+            return;
+        }
+        if (mapKeyword(endStr) == Token::FALSE) {
+            addToken(Token::FALSE, 0.0);
+            return;
+        }
+        addToken(mapKeyword(endStr), endStr);
+    }
+    else {
+        addToken(Token::IDENTIFIER, endStr);
+    }
+}
+static void getToken() {
+
+    char LastChar = advance();
+
+    // Skip any whitespace.
+    switch (LastChar)
+    {
+    case '+':addToken(Token::PLUS, "+"); break;
+    case '-':addToken(Token::MINUS, "-"); break;
+    case '*':addToken(Token::STAR, "*"); break;
+    case '/':addToken(Token::SLASH, "/"); break;
+    case '=':addToken(Token::EQUAL, "="); break;
+    case '{':addToken(Token::LBRACE, "{"); break;
+    case '}':addToken(Token::RBRACE, "}"); break;
+    case '(':addToken(Token::LPAREN, "("); break;
+    case ')':addToken(Token::RPAREN, ")"); break;
+    case ';':
+        addToken(Token::EOL, ";");
+        line++;
+        break;
+    case ' ':break;
+    case '\t':break;
+    case '\r':break;
+    case '"':str(); break;
+    default:
+        if (isdigit(LastChar))
+        {
+            num(LastChar);
+        }
+        else if (isalnum(LastChar))
+        {
+            identifier(LastChar);
+        }
+        else {
+
+            std::cout << int(LastChar);
+            std::cout << "UNEXPECTED CHAR!\n";
+        }
+        break;
+    }
+}
+int main(int argc, char* argv[]) {
+
+    while (true) {
+        current = 0;
+        line = 0;
+        TokenStream.clear();
+        std::cout << "> ";
+        getline(std::cin, source);
+        getToken();
+        while (!isAtEnd())
+        {
+            getToken();
+        }
+        for (size_t i = 0; i < TokenStream.size(); i++)
+        {
+            std::cout << TokenStream[i].toString() << "\n";
+        }
+    }
+    return 0;
+
+}
--- dumblang/Token.cpp
+++ dumblang/Token.cpp
@@ -5,8 +5,49 @@
 #include <map>
 #include <string>
 
-std::string stringify(std::variant<double, std::string> const& value) {
-	if (double const* pval = get_if<double>(&value))
-		return std::to_string(*pval);
-	return get<std::string>(value);
-};
+
+std::string Token::stringify(std::variant<double, std::string> const& value) {
+    if (double const* pval = get_if<double>(&value))
+        return std::to_string(*pval);
+    return get<std::string>(value);
+}
+std::string Token::toString() {
+	return "type:" + mapToken(type) + " |lexeme: " + stringify(lexeme) + " |line:" + std::to_string(line);
+}
+std::map<std::string, Token::Type> GetKeywordMap() {
+    std::map<std::string, Token::Type> KeywordMap = {
+       {"print",Token::PRINT},
+       {"input",Token::INPUT}
+    };
+    return KeywordMap;
+}
+
+std::map<Token::Type, std::string> GetTokenMap() {
+    std::map<Token::Type, std::string> TokenMap = {
+       {Token::PRINT,"PRINT"},
+       {Token::INPUT,"INPUT"},
+       {Token::STR,"STR"},
+       {Token::NUM,"NUM"},
+       {Token::IDENTIFIER,"IDENTIFIER"},
+       {Token::EOL,"EOL"},
+       {Token::PLUS,"PLUS"},
+       {Token::MINUS,"MINUS"},
+       {Token::STAR,"STAR"},
+       {Token::SLASH,"SLASH"},
+       {Token::LPAREN,"LPAREN"},
+       {Token::RPAREN,"RPAREN"},
+       {Token::LBRACE,"LABRACE"},
+       {Token::RBRACE,"RBRACE"},
+       {Token::EQUAL,"EQUAL"},
+       {Token::EOFL,"EOFL"}
+    };
+    return TokenMap;
+}
+Token::Type mapKeyword(std::string keyword) {
+    std::map <std::string, Token::Type> KeywordMap = GetKeywordMap();
+    return KeywordMap[keyword];
+}
+std::string mapToken(Token::Type tk) {
+    std::map < Token::Type, std::string> TokenMap = GetTokenMap();
+    return TokenMap[tk];
+}

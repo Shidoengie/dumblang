0ff12452f00d81b1581d988ab7246108b08dc196
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2023-03-30 14:09:44 +0100

    rewrote ast added eval function

--- dumblang/AST.cpp
+++ dumblang/AST.cpp
@@ -1,82 +1,55 @@
+
 #include "AST.h"
 #include "Token.h"
 #include <iostream>
 #include <vector>
+#include <variant>
 
-//https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/LangImpl02.html
-
-
-class Expression{
-public:
-	virtual ~Expression() = default;
+static int count = 0;
+enum operatorType {
+	ADD,
+	SUBTRACT,
+	DIVIDE,
+	MULTIPLY,
+	ASSIGN
 };
-typedef std::unique_ptr<Expression> ExprPtr;
-class NumberExpr : public Expression{
-	Token* tokenObj;
-public:
-	NumberExpr(Token* newToken) : tokenObj(newToken) {}
+struct Variable;
+struct BinaryExpr;
+typedef std::variant<double, BinaryExpr, Variable> Expression;
+
+struct BinaryExpr {
+	operatorType type;
+	Expression* left;
+	Expression* right;
 };
-class VariableExpr : public Expression {
-	std::string varName;
-
-public:
-	VariableExpr(const std::string& newName) : varName(newName) {}
+struct Variable
+{
+	std::string name;
 };
-
-/// BinaryExprAST - Expression class for a binary operator.
-class BinaryExpr : public Expression {
-	Token::Type operatorType;
-	ExprPtr Left, Right;
-	// v init list v
-	BinaryExpr(Token::Type opType, ExprPtr leftObj, ExprPtr rightObj)
-		: operatorType(opType), Left(std::move(leftObj)), Right(std::move(rightObj)) {}
-	// ^ init list ^
-};
-class AdditionExpr : public BinaryExpr {
-
-};
-class MultExpr : public BinaryExpr {
-
-};
-class DivExpr : public BinaryExpr {
-
-};
-class SubExpr : public BinaryExpr {
-
-};
-
-class UnaryExpr : public Expression {
-	Token::Type operatorType;
-	ExprPtr Object;
-	UnaryExpr(Token::Type opType, ExprPtr assignedObj)
-		: operatorType(opType), Object(std::move(assignedObj)){}
-};
-
-class CallExpr : public Expression {
-	std::string calleeName;
-	std::vector<ExprPtr> callArgs;
-
-public:
-	CallExpr(const std::string& Callee, std::vector<ExprPtr> Args)
-		: calleeName(Callee), callArgs(std::move(Args)) {}
-};
-class PrototypeExpr {
-	std::string protoName;
-	std::vector<std::string> protoArgs;
-
-public:
-	PrototypeExpr(const std::string& Name, std::vector<std::string> Args)
-		: protoName(Name), protoArgs(std::move(Args)) {}
-
-	const std::string& getName() const { return protoName; }
-};
-
-class FunctionExpr {
-	typedef std::unique_ptr<PrototypeExpr> ProtoPtr;
-	ProtoPtr funcProto;
-	ExprPtr funcBody;
-
-public:
-	FunctionExpr(ProtoPtr Proto, ExprPtr Body)
-		: funcProto(std::move(Proto)), funcBody(std::move(Body)) {}
+double eval(Expression expr) {
+	if (std::holds_alternative<double>(expr)) {
+		return std::get<double>(expr);
+	}
+	else {
+		BinaryExpr binOp = std::get<BinaryExpr>(expr);
+		double left_value = eval(*binOp.left);
+		double right_value = eval(*binOp.right);
+		switch (binOp.type)
+		{
+		case ADD:
+			return left_value + right_value;
+			break;
+		case MULTIPLY:
+			return left_value * right_value;
+			break;
+		case DIVIDE:
+			return left_value / right_value;
+			break;
+		case SUBTRACT:
+			return left_value - right_value;
+			break;
+		default:
+			break;
+		}
+	}
 };

28cf3f1a1391e1f15992a98273b541d889246c0c
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2023-04-05 16:30:21 +0100

    finished error handling

--- dumblang/AST.cpp
+++ dumblang/AST.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <variant>
 #include <exception>
-static int count = 0;
+#include <map>
 enum operatorType {
 	ADD,
 	SUBTRACT,
@@ -25,54 +25,53 @@ struct Variable {
 	std::string name;
 };
 std::map<std::string, double> varMap = {
-	{"Test",10.0}
+	{"Test",2.0},
+	{"PI",3.146210}
 };
 
 double Eval(Expression expr) {
 	if (std::holds_alternative<double>(expr)) {
 		return std::get<double>(expr);
 	}
-	else if (std::holds_alternative<Variable>(expr)) {
+	if (std::holds_alternative<Variable>(expr)) {
 		Variable var = std::get<Variable>(expr);
 		if (varMap.contains(var.name)) {
 			return varMap[var.name];
 		}
-		else { 
-			throw "Undeclared Variable";
-		}
+		throw "Undeclared Variable";
 	}
-	else {
-		BinaryExpr binOp = std::get<BinaryExpr>(expr);
-		double leftValue, rightValue;
-		try {
-			leftValue = Eval(*binOp.left);
-		}
-		catch (const char* msg) {
-			std::cout << msg << '\n';
-		}
-		try {
-			rightValue = Eval(*binOp.right);
-		}
-		catch (const char* msg) {
-			std::cout << msg << '\n';
-		}
-		switch (binOp.type)
-		{
-		case ADD:
-			return leftValue + rightValue;
-			break;
-		case MULTIPLY:
-			return leftValue * rightValue;
-			break;
-		case DIVIDE:
-			return leftValue / rightValue;
-			break;
-		case SUBTRACT:
-			return leftValue - rightValue;
-			break;
-		default:
-			std::cout << "idk yet";
-			break;
-		}
+	BinaryExpr binOp = std::get<BinaryExpr>(expr);
+	double leftValue, rightValue;
+	try {
+		leftValue = Eval(*binOp.left);
+	}
+	catch (const char* msg) {
+		throw msg;
+	}
+	try {
+		rightValue = Eval(*binOp.right);
+	}
+	catch (const char* msg) {
+		throw msg;
+	}
+
+	switch (binOp.type)
+	{
+	case ADD:
+		return leftValue + rightValue;
+		break;
+	case MULTIPLY:
+		return leftValue * rightValue;
+		break;
+	case DIVIDE:
+		return leftValue / rightValue;
+		break;
+	case SUBTRACT:
+		return leftValue - rightValue;
+		break;
+	default:
+		std::cout << "idk yet";
+		break;
+
 	}
 };

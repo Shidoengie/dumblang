7bb038d2e773423e3d21fac2b7247d04b80820e3
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2023-05-18 15:00:21 +0100

    better debugging

--- dumblang/range.cpp
+++ dumblang/range.cpp
@@ -15,14 +15,14 @@ Range::Range(size_t max_, size_t start_, size_t stop_) {
     }
     if (start_ > max_ || stop_ > max_) {
         std::ostringstream out;
-        out << "range greater then max Range: "this->to_string();
+        out << "range greater then max Range: " << "max:" << max_ << " start:" << start_ << " stop:" << stop_;
         throw InvalidRangeError(out.str());
     }
     max = max_;
     start = start_;
     stop = stop_;
 }
-std::string Range::to_string(Range range) {
+std::string Range::to_string() {
     std::ostringstream out;
     out << "max:" << max << " start:"<<start<<" stop:"<<stop;
     return out.str();

30f54f7a95df893ad550a42e81e2044562ba7fa4
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2023-05-24 18:47:34 +0100

    improved parser

--- dumblang/Parser.cpp
+++ dumblang/Parser.cpp
@@ -2,9 +2,11 @@
 #include "Defaults.h"
 static std::string source;
 static std::vector<Token> stream;
-int current = 0;
+static std::vector<Node> body;
+int index = 0;
 
 using Kind = Token::Type;
+using TokenResult = std::optional<Token>;
 
 std::string text(Token token) {
 	auto res = token.lexeme.splice(source);
@@ -13,27 +15,88 @@ std::string text(Token token) {
 	}
 	throw UnspecifiedError("idk");
 }
-Kind Advance() {
-	current++;
-	Kind token = stream[current].type;
-	return token;
+bool AtEnd() {
+	if (index + 1 > stream.size()) {
+		return true;
+	}
+	return false;
 }
-Kind Peek() {
-	return stream[current].type;
+Token Peek() {
+	return stream[index];
 }
-bool CurrentHas(Kind expected) {
-	return Peek() == expected;
+TokenResult PeekNext() {
+	if (!AtEnd()) {
+		return stream[index + 1];
+	}
+	return {};
+}
+Token Advance() {
+	
+	if (!AtEnd()) {
+		index++;
+	}
+	Token next = Peek();
+	return next;
+}
+
+bool matches(Kind expected) {
+	auto next = PeekNext();
+	if (!next || (*next).type != expected) {
+		return false;
+	}
+	index++;
+	return true;
+}
+Node ParseVar() {
+	
+	if (!matches(Token::IDENTIFIER)) {
+		throw UnspecifiedError("Invalid syntax");
+	}
+	std::string name = text(Peek());
+	if (matches(Token::EOL)) {
+		NodePtr val = std::make_unique<Node>(0.0);
+		Node decl = Declaration(name,std::move(val));
+		return decl;
+	}
+	if (!matches(Token::EQUAL)) {
+		throw UnspecifiedError("Invalid syntax");
+	}
 }
 Node ParseTokens() {
-	auto left = Peek();
-	switch (left) {
-
-	case Token::NUM:
-
+	auto start = Peek();
+	switch (start.type)
+	{
+	case Token::VAR: {
+		
+		
+		break;
+	}	
+	case Token::FUNC: {
+		
+		break;
+	}
 	default:
 		break;
 	}
+	
+	return NoneType();
 }
-void ParseNumRange(Token literal) {
-	literal.lexeme.splice(source);
+
+Node ParseValue(Token literal) {
+	auto tokenVal = text(literal);
+	switch (literal.type) {
+	case Token::STR: 
+		return Value(tokenVal);
+	case Token::NUM: {
+		double value = std::stod(tokenVal);
+		Node number = Value(value);
+		return number;
+	}
+	case Token::IDENTIFIER: {
+		
+	}
+	default:
+		break;
+	}
+	return NoneType();
 }
